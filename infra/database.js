0cb52b18384456de079901a655347d64045b9ec7
João Vítor Gasparoni <81528139+jvgasparoni@users.noreply.github.com>
2025-08-25 19:27:41 +0000

    mudanças em testes e database.js

--- infra/database.js
+++ infra/database.js
@@ -1,25 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
+  let client;
   try {
+    client = await getNewClient();
     await client.connect();
     const result = await client.query(queryObject);
     return result;
@@ -29,6 +13,19 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+async function getNewClient(){
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+  await client.connect();
+  return client;
 }
 
 export default {
